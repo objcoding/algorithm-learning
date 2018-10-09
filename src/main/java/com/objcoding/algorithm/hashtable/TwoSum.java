933b87b4f40874227553cee8d32a20d6821b8013
zch <zhangchenghui.dev@gmail.com>
2018-10-09 20:10:05 +0800

    twosum

---
+++ src/main/java/com/objcoding/algorithm/hashtable/TwoSum.java
@@ -0,0 +1,48 @@
+package com.objcoding.algorithm.hashtable;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 两数之和
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ * 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ *
+ * 难度：简单
+ *
+ * 示例：给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class TwoSum {
+
+    /**
+     * 1.一遍哈希表
+     * 时间复杂度：O(n)
+     * 把遍历过的元素存进map的key中，value则是元素在数组中对应的下标，
+     * 遍历时，判断当前元素与map中key相加是否等于target，等于则返回对应下标
+     *
+     * 关键点在于查找map中key用了hash表，查找时间复杂度只有O(1)
+     *
+     * 执行用时：8 ms
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
