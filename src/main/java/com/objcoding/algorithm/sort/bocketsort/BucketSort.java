1149c0df5f7f234a4c735df109239e3e390b45a1
zch <zhangchenghui.dev@gmail.com>
2018-10-26 00:39:09 +0800

    bucket sort

---
+++ src/main/java/com/objcoding/algorithm/sort/bocketsort/BucketSort.java
@@ -0,0 +1,22 @@
+package com.objcoding.algorithm.sort.bocketsort;
+
+/**
+ * 桶排序
+ *
+ * @author zhangchenghui.dev@gmail.com
+ * @since 2018/10/24
+ */
+public class BucketSort {
+
+
+    /**
+     * 桶排序的原理是将数据分割成一个个桶，每个桶都是一个数值范围，
+     * 比如：100个数值排序，可以分成10个桶，数值0-10对应第一个桶，编号01，如此类推
+     * 然后再每个桶内部用快速排序算法进行排序
+     */
+
+
+    public static void bucketSort() {
+
+    }
+}
